c9af5b6b2d00e632f84e58417812226034b12fb3
Hemanth <hemanthtinkuz@gmail.com>
2024-10-20 11:13:57 +0530

    added: Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import ProfileForm from './pages/ProfileForm';
+import Profile from './pages/Profile';
+import PageNotFound from './pages/PageNotFound';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/profile-form" component={ProfileForm} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/404" component={PageNotFound} />
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
